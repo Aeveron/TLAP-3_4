32345ff795f61c92d86e24a5cee8e30597cd3c3f
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-13 13:27:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TLAP_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+            Console.WriteLine(alphabet);
+        }
+    }
+}
