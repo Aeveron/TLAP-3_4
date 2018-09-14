86c76945036f9bd814a5ba5acd4ae8d9ec8e75fd
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-14 09:33:21 +0200

    x
    x

--- Program.cs
+++ Program.cs
@@ -6,14 +6,43 @@ namespace TLAP_3_4
     {
         static void Main(string[] args)
         {
-            MainOutput();
+            MainOutput(args[0] == "e", args[1]);
         }
 
-        private static void MainOutput()
+        private static void MainOutput(bool encrypt, string plaintext)
         {
             var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
             var code = "QWERTYUIOPÅÆØLKJHGFDSAZXCVBNM";
-            Console.WriteLine(alphabet);
+            var cipherText = Encrypting(plaintext,
+                encrypt ? alphabet : code,
+                encrypt ? code : alphabet);
+
+            Console.WriteLine(cipherText);
+
+        }
+
+        private static string Encrypting(string plaintext, string alphabet, string code)
+        {
+            var cipherText = "";
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                cipherText += EncryptChar(plaintext[i], alphabet, code);
+            }
+
+            return cipherText;
+        }
+
+        private static char EncryptChar(char c, string alphabet, string code)
+        {
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (alphabet[i] == char.ToUpper(c))
+                {
+                    return code[i];
+                }
+            }
+
+            return c;
         }
     }
 }
